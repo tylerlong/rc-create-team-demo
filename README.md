e6884ccd5875f465fade6bff615e839ad1e0e374
Tyler Long <tyler4long@gmail.com>
2023-03-10 09:47:07 -0800

    First fully working version

--- README.md
+++ README.md
@@ -1,1 +1,18 @@
 # RC Create Team Demo
+
+## Setup
+
+```
+yarn install
+```
+
+Rename `.env.sample` to `.env`.
+
+Edit `.env` to specify credentials.
+
+
+## Run
+
+```
+yarn test
+```
--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,25 @@
-console.log('Hello world!');
+import RingCentral from '@rc-ex/core';
+
+const rc = new RingCentral({
+  clientId: process.env.RINGCENTRAL_CLIENT_ID,
+  clientSecret: process.env.RINGCENTRAL_CLIENT_SECRET,
+  server: process.env.RINGCENTRAL_SERVER_URL,
+});
+
+const main = async () => {
+  await rc.authorize({
+    jwt: process.env.RINGCENTRAL_JWT_TOKEN!,
+  });
+
+  const r = await rc
+    .teamMessaging()
+    .teams()
+    .post({
+      public: true,
+      name: 'Fun team',
+      members: [{ email: 'member.1@gmail.com' }, { email: 'member.2@gmail.com' }],
+      description: "Let's chit chat here",
+    });
+  console.log(r);
+};
+main();

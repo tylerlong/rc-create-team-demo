5971f8827152f94052dccf51cbca3168c70e1dba
Tyler Long <tyler4long@gmail.com>
2023-03-14 09:32:18 -0700

    list all teams

--- src/index.ts
+++ src/index.ts
@@ -11,15 +11,22 @@ const main = async () => {
     jwt: process.env.RINGCENTRAL_JWT_TOKEN!,
   });
 
-  const r = await rc
-    .teamMessaging()
-    .teams()
-    .post({
-      public: true,
-      name: 'Fun team',
-      members: [{ email: 'member.1@gmail.com' }, { email: 'member.2@gmail.com' }],
-      description: "Let's chit chat here",
-    });
-  console.log(r);
+  // list all teams
+  const r = await rc.teamMessaging().teams().list({ recordCount: 20 });
+  for (const team of r.records ?? []) {
+    console.log(team.name, '-', team.description);
+  }
+
+  // create a team
+  // const r = await rc
+  //   .teamMessaging()
+  //   .teams()
+  //   .post({
+  //     public: true,
+  //     name: 'Fun team',
+  //     members: [{ email: 'member.1@gmail.com' }, { email: 'member.2@gmail.com' }],
+  //     description: "Let's chit chat here",
+  //   });
+  // console.log(r);
 };
 main();

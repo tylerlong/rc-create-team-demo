65435200632e6ddb78b05334daa170002782baa5
Tyler Long <tyler4long@gmail.com>
2023-03-23 09:17:59 -0700

    List all chats

--- src/index.ts
+++ src/index.ts
@@ -7,15 +7,26 @@ const rc = new RingCentral({
 });
 
 const main = async () => {
+  // jwt flow
   await rc.authorize({
     jwt: process.env.RINGCENTRAL_JWT_TOKEN!,
   });
 
-  // list all teams
-  const r = await rc.teamMessaging().teams().list({ recordCount: 20 });
-  for (const team of r.records ?? []) {
-    console.log(team.name, '-', team.description);
-  }
+  const r = await rc.get('/restapi/v1.0/glip/chats');
+  console.log(r.data);
+
+  // // OR password flow
+  // await rc.authorize({
+  //   username: '',
+  //   extension: '',
+  //   password: '',
+  // });
+
+  // // list all teams
+  // const r = await rc.teamMessaging().teams().list({ recordCount: 20 });
+  // for (const team of r.records ?? []) {
+  //   console.log(team.name, '-', team.description);
+  // }
 
   // create a team
   // const r = await rc
